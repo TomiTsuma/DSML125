02ac5c0bc1dfea9338970a64f7b2602c3c6d9a18
TomiTsuma <tommytsuma7@gmail.com>
2024-05-14 09:43:32 +0000

    edited r install script

--- r_installation.sh
+++ r_installation.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install r-base
+
 
 wget https://cran.r-project.org/src/contrib/clhs_0.9.0.tar.gz
 wget https://cran.r-project.org/src/contrib/Archive/terra/terra_1.7-28.tar.gz
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo apt update
+sudo apt install r-base
+sudo apt-get install r-base
+R --version
 sudo apt install libudunits2-dev
 sudo apt install libgdal-dev
 sudo apt install r-base-core
